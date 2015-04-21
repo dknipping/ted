d62a924a5eb2a036a652e30378219a42b4fe429a
Daniel Knipping <daniel.knipping@prodyna.com>
2015-04-21 20:16:19 +0200

    initial commit

---
+++ desk/src/main/java/com/ted/cockpit/desk/Application.java
@@ -0,0 +1,16 @@
+package com.ted.cockpit.desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
