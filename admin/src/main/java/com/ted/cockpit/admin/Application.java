337d887061279cfc18b99f0362681f4e01651827
Reiner Hoppe <reiner.hoppe@prodyna.com>
2015-06-12 13:52:20 +0200

    spring-boot-admin integriert

--- admin/src/main/java/com/ted/cockpit/admin/Application.java
+++ admin/src/main/java/com/ted/cockpit/admin/Application.java
@@ -1,11 +1,14 @@
-package com.ted.cockpit.desk;
+package com.ted.cockpit.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
 public class Application {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
